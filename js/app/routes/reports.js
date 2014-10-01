232f50401243d814785551e19fb00c951ad3787f
toran billups <toranb@gmail.com>
2014-09-30 21:44:14 -0500

    step 3: now lazy loading a model/controller/view

--- js/app/routes/reports.js
+++ js/app/routes/reports.js
@@ -1,10 +1,6 @@
 var ReportsRoute = Ember.Route.extend({
     beforeModel: function() {
         return $.getScript('js/lazy/reporting.js');
-    },
-    afterModel: function() {
-        var Foo = require('vendor/foo')['default'].create();
-        Foo.get('help');
     }
 });
 
--- js/lazy/reporting.js
+++ js/lazy/reporting.js
@@ -1,11 +1,36 @@
-define("vendor/foo",
+define("js/models/report",
   ["exports"],
   function(__exports__) {
     "use strict";
-    var Foo = Ember.Object.extend({
-        help: function() {
-            console.log('inside the help property');
+    var Report = Ember.Object.extend({
+        foo: function() {
+            return 'foo...';
         }.property()
     });
-    __exports__["default"] = Foo;
+    __exports__["default"] = Report;
+});
+
+define("js/controllers/reports",
+  ["js/models/report","exports"],
+  function(__dependency1__, __exports__) {
+    "use strict";
+    var Report = __dependency1__["default"];
+    var ReportsController = Ember.ArrayController.extend({
+        bar: function() {
+            var model = Report.create();
+            var foo = model.get('foo');
+            return '%@ and ... bar'.fmt(foo);
+        }.property()
+    });
+    __exports__["default"] = ReportsController;
+});
+
+define("js/views/reports",
+  ["exports"],
+  function(__exports__) {
+    "use strict";
+    var ReportsView = Ember.View.extend({
+        defaultTemplate: Ember.Handlebars.compile('<h3>Hi</h3>{{foo}} and {{bar}}')
+    });
+    __exports__["default"] = ReportsView;
 });
