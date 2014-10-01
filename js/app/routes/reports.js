79f7767ab52908c2f1c8fb25d532e5b01acf4d27
toran billups <toranb@gmail.com>
2014-09-30 19:33:14 -0500

    step 2: lazy loading an AMD module

--- js/app/routes/reports.js
+++ js/app/routes/reports.js
@@ -1,6 +1,10 @@
 var ReportsRoute = Ember.Route.extend({
     beforeModel: function() {
         return $.getScript('js/lazy/reporting.js');
+    },
+    afterModel: function() {
+        var Foo = require('vendor/foo')['default'].create();
+        Foo.get('help');
     }
 });
 
--- js/lazy/reporting.js
+++ js/lazy/reporting.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+define("vendor/foo",
+  ["exports"],
+  function(__exports__) {
+    "use strict";
+    var Foo = Ember.Object.extend({
+        help: function() {
+            console.log('inside the help property');
+        }.property()
+    });
+    __exports__["default"] = Foo;
+});
